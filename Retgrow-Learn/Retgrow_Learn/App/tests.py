36fe75653691cc40090aa882c91f7d42d625c98f
Ibrahim A. Makanjuola <145908520+Twilight-Techie-X@users.noreply.github.com>
2024-08-13 14:35:46 +0100

    Delete Retgrow-Learn directory

--- Retgrow-Learn/Retgrow_Learn/App/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-from django.contrib.auth.models import User
-from .models import Course, Task
-
-# Create your tests here.
-from django.test import TestCase
-
